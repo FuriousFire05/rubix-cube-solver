50c1de57718dcc7fb2b53e98f956b6037c364ac2
FuriousFire05 <furiousfire05@gmail.com>
2025-04-24 13:27:05 +0400

    Deleted C++ files for Python implementation

--- include/rubiks_solver.h
+++
@@ -1,7 +0,0 @@
-// rubiks_solver.h
-#ifndef RUBIKS_SOLVER_H
-#define RUBIKS_SOLVER_H
-
-void solveRubiksCube(); // Function to solve the Rubik's Cube
-
-#endif // RUBIKS_SOLVER_H
--- src/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Rubik's Cube Solver - Let's go!" << std::endl;
-    return 0;
-}
