fd96265559068699ff030de79b39ae5fa16032d8
FuriousFire05 <furiousfire05@gmail.com>
2025-04-27 15:29:59 +0400

    Tiny fix on input arguments

--- tests/test_pieces.py
+++ tests/test_pieces.py
@@ -15,7 +15,7 @@ from utils.faces import Face
 def test_piece_initialization():
     colors = {Face.U: Color.YELLOW}
     position = (0, 1, 0)
-    piece = Piece(colors, position)
+    piece = Piece(colors, "", position)
 
     assert piece.get_position() == position
     assert piece.get_faces() == colors
@@ -24,7 +24,7 @@ def test_piece_initialization():
 def test_center_initialization():
     colors = {Face.U: Color.YELLOW}
     position = (0, 1, 0)
-    center = Center(colors, position)
+    center = Center(colors, "", position)
 
     assert center.get_position() == position
     assert center.get_faces() == colors
@@ -33,7 +33,7 @@ def test_center_initialization():
 def test_edge_initialization():
     colors = {Face.F: Color.BLUE, Face.R: Color.RED}
     position = (1, 0, 1)
-    edge = Edge(colors, position)
+    edge = Edge(colors, "", position)
 
     assert edge.get_position() == position
     assert edge.get_faces() == colors
@@ -42,7 +42,7 @@ def test_edge_initialization():
 def test_corner_initialization():
     colors = {Face.U: Color.YELLOW, Face.F: Color.BLUE, Face.R: Color.RED}
     position = (1, 1, 1)
-    corner = Corner(colors, position)
+    corner = Corner(colors, "", position)
 
     assert corner.get_position() == position
     assert corner.get_faces() == colors
