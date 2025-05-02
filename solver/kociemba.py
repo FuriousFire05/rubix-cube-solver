12b0ffbe5c19a8feefdb4e469bcb2fedaf73ec9d
FuriousFire <136786792+Furious-Fire@users.noreply.github.com>
2025-05-02 19:54:08 +0400

    Added Kociemba Solver, Working on other Solving algorithms

---
+++ solver/kociemba.py
@@ -0,0 +1,11 @@
+# solver/kociemba.py
+
+import kociemba
+from core.cube import RubiksCube
+
+class Kociemba_Solver:
+    def __init__(self, cube: RubiksCube):
+        self.cube_string = cube.toString()
+
+    def get_solution(self):
+        return kociemba.solve(self.cube_string)
